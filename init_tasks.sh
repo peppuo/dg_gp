61a95387c69ab8e80b1c31aa266793851e7db5b3
peppuo <57544770+peppuo@users.noreply.github.com>
2019-11-11 23:30:37 +0000

    put all stuff in init.sh to .gitpod.yml

--- init_tasks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo 'alias pip="pip3"' >> ~/.bashrc
-echo 'alias python="python3"' >> ~/.bashrc
-echo 'alias run="python3 manage.py runserver $IP:$PORT"' >> ~/.bashrc
-source ~/.bashrc
-
-pip3 install --upgrade pip
-pip3 install -r requirements.txt
