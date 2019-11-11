beeffd150114bd890b9ffb77e154aa07a9c67b2a
peppuo <peppujool@hotmail.com>
2019-11-11 23:01:25 +0000

    added source cmd

--- init_tasks.sh
+++ init_tasks.sh
@@ -3,6 +3,7 @@
 echo 'alias pip="pip3"' >> ~/.bashrc
 echo 'alias python="python3"' >> ~/.bashrc
 echo 'alias run="python3 manage.py runserver $IP:$PORT"' >> ~/.bashrc
+source ~/.bashrc
 
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
