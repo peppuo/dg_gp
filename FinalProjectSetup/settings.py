ef0b9d76c7259e273ca3338c16d66f8ced1b1da7
peppuo <peppujool@hotmail.com>
2019-11-13 02:11:25 +0000

    set debug mode using environmental variable

--- FinalProjectSetup/settings.py
+++ FinalProjectSetup/settings.py
@@ -26,7 +26,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  # TODO
+if os.getenv('DEVELOPMENT') == 'true':
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = [
     os.getenv('GP_HOSTNAME'),   # GitPod
